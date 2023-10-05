a8bc29148e453c8090a9351bdcdadcbec5c2c672
Dan Chui <dchui108@gmail.com>
2023-10-05 18:22:09 +0900

    add Actions for Register form

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -106,7 +106,8 @@
 </template>
 
 <script>
-import { auth } from '@/includes/firebase'
+import { mapActions } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'RegisterForm',
@@ -131,15 +132,17 @@ export default {
     }
   },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: 'register'
+    }),
     async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_msg = 'Please wait! Your Account is being Created.'
 
-      let userCred = null
       try {
-        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
+        await this.createUser(values)
       } catch (error) {
         this.reg_in_submission = false
         this.reg_alert_variant = 'bg-red-500'
@@ -149,7 +152,6 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
-      console.log(userCred)
     }
   }
 }
