073097dda504c7ea03767d637a3de905b38d150b
Dan Chui <dchui108@gmail.com>
2023-10-13 22:14:14 +0900

    Feat play and toggle Audio

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -8,6 +8,7 @@
     <div class="container mx-auto flex items-center">
       <!-- Play/Pause Button -->
       <button
+        @click.prevent="newSong(song)"
         type="button"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
       >
@@ -85,9 +86,10 @@
 
 <script>
 import { songsCollection, auth, commentsCollection } from '@/includes/firebase'
-import { mapState } from 'pinia'
+import { mapState, mapActions } from 'pinia'
 import useUserStore from '@/stores/user'
 import { ErrorMessage } from 'vee-validate'
+import usePlayerStore from '@/stores/player'
 
 export default {
   name: 'Song',
@@ -133,6 +135,7 @@ export default {
     this.getComments()
   },
   methods: {
+    ...mapActions(usePlayerStore, ['newSong']),
     async addComment(values, { resetForm }) {
       this.comment_in_submission = true
       this.comment_show_alert = true
