0d591169577965d76d4e81056c8812a6aed9f7b2
Dan Chui <dchui108@gmail.com>
2023-10-17 11:01:54 +0900

    Feat Auto register global components

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.93fivmb5nko"
+    "revision": "0.rqp6ds7fhbg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
